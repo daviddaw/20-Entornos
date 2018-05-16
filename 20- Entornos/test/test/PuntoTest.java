abbc297bf31252f17a882193e794a3d4181bbb86
Daveto <David.palanco@gmail.com>
2018-05-16 03:12:06 +0200

    sdwef

--- 20- Entornos/test/test/PuntoTest.java
+++ 20- Entornos/test/test/PuntoTest.java
@@ -1,20 +1,65 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import paquete.Punto;
+
 public class PuntoTest {
+	Punto punto1;
+	Punto punto2;
+	Punto punto3;
+	Punto punto4;
+	
 
 	@Before
 	public void setUp() throws Exception {
-		//crear varios puntos y varios metodos que validen distancia y desplazar
+		punto1=new Punto();
+		punto2=new Punto(-1, -1);
+		punto3=new Punto(-4, 0);//-5,0
+		punto4=new Punto(7,5);//4,1
+		//crear varios puntos 
+	}
+	//y varios metodos que validen distancia y desplazar
+	@Test
+	public void testDesplazar2() {
+		punto2.desplazar(2);
+		assertTrue( punto2.getX()==1.0 && punto2.getY()==1.0);
+		
+	}
+	
+	@Test
+	public void testDesplazar1() {
+		punto1.desplazar(5);
+		assertTrue( punto1.getX()==5 && punto1.getY()==5);
+		
+	}
+	
+	
+	
+	
+	@Test
+	public void testDidtancia() {
+		Punto otro= new Punto(2,3);
+		assertTrue(punto2.distancia(otro)==5);
+		
+	}
+	@Test
+	public void testDistancia3	() {
+		Punto otro= new Punto(-5,0);
+		
+		assertTrue(punto3.distancia(otro)==1.0);
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDistancia4	() {
+		Punto otro= new Punto(4,1);
+		assertTrue(punto4.distancia(otro)==5.0);
+		
 	}
-
+	
+	
 }
